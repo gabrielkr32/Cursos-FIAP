7a884dadcf63a2a90468c0bbfb6c26c6894eab5e
Gabriel Kruger <gabrielkr32@gmail.com>
2024-03-04 22:23:05 -0300

    Recuperacao dos dados de arquivos/string

--- Python/Capit4_Dicionarios/funcao_arquivo.py
+++ Python/Capit4_Dicionarios/funcao_arquivo.py
@@ -1,4 +1,6 @@
 def chamarMenu():
+    #a funcao apenas exibi para usuario as opcaoes que ele possui, para manipulaçao do inventario
+      #e retorna o valor sewlecionado por meio da variavel ' escolha '
     escolha = int(input("""Digite: 
                   <1> registrar ativo
                    <2> persistir arquivo
@@ -6,6 +8,8 @@ def chamarMenu():
     return escolha
 
 def registrar(dicionario):
+    #a funcao ia receber o dicionario responsavel por armazenar os ativos e entao
+      #ira preencher quanto o usuario digitar 'S'
     resposta = "S"
     while resposta == "S":
         dicionario[input("Digite o numero patrimoniAL: ")]=[
@@ -15,6 +19,7 @@ def registrar(dicionario):
         resposta("Digite <5> para continuar. ").upper()
 
 def persistir (dicionario):
+     #descarregar o diconario que for passado com parametro para dentro do arquivo 'inventario.csv' 
      with open("inventario.csv", "a") as inv:
          for chave, valor in dicionario.items():
              inv.write(chave +";" + valor[0] + ":" +
@@ -22,7 +27,7 @@ def persistir (dicionario):
 
              return "Persistido com sucesso!" 
 
-def exibir():
+def exibir():#retornar o conteudos das linhas do 'inventario.csv' por meio da variavel 'linha'
     with open("Inventario.csv", "r") as inv:
         linhas = inv.readlines()
         return linhas
---
+++ Python/Capit5_Manipulação de arquivos e JSON/Inventario003.py
@@ -0,0 +1,18 @@
+from Capit4_Dicionarios.funcao_arquivo import *
+inventario={}
+opcao=chamarMenu()
+while opcao>0 and opcao<4:
+    if opcao==1:
+        registrar(inventario)
+    elif opcao==2:
+        persistir(inventario)
+    elif opcao==3:
+        resultado = exibir()
+        for linha in resultado:
+            print(linha[2:-1])
+    opcao = chamarMenu()
+
+
+    #de toda a linha, iremos exibir somente do terceiro caracter [2] ate o ultimo [-1]
+    #por meio do [2: -1] que se cahma 'slice' e representa um corte em um strig
+    
